ffe480c6f135b8774698879fe334f9e3b90d2a4a
Adam Marsala <adammarsala@hotmail.com>
2021-04-01 22:16:09 +0000

    Add create logic for users controller, enable bcrypt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,23 @@ class UsersController < ApplicationController
   # the 'index' view will give us the login form
   def index
   end
-  
+
   # the 'new' view will give us the sign up form
   def new
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
   end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :last_name, :email, :password, :password_confirmation)
+    end
 end
