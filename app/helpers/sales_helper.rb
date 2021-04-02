735d144b899fbdbb1650bc857754d6edc9a94c05
Adam Marsala <adammarsala@hotmail.com>
2021-04-02 01:33:09 +0000

    Define sale helpers and class methods for Sale model to show current active sale

--- app/helpers/sales_helper.rb
+++ app/helpers/sales_helper.rb
@@ -1,8 +1,17 @@
 module SalesHelper
   #The helper modules in the helpers folder can be referenced anywhere, this one is used in the layout
   def active_sale?
-    # inside the where clause we are actually writing SQL, since its Action Record, it needs two arguments since we use two ?'s
-    Sale.where("sales.starts_on <= ? AND sales.ends_on >= ?", 
-    Date.current, Date.current).any?
+    # Here we reference the custom active method in our Sale Model in sale.rb, and chain an any? SQL AR method
+    Sale.active.any?
   end
+
+  def sale_name
+    Sale.get_sale.name
+  end
+
+  def sale_percent_off
+    Sale.get_sale.percent_off
+  end
+
+
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,22 @@
 class Sale < ActiveRecord::Base
+
+  # inside the where clause we are actually writing SQL, since its Action Record, it needs two arguments since we use two ?'s
+  #@@active_sale_query = "sales.starts_on <= ? AND sales.ends_on >= ?"
+
+  # AR Scope - Class Methods (starting with self.), we can reference any methods we make here in our helpers
+  def self.active
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current)
+    #self.where is implied
+  end
+
+  def self.get_sale
+    where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current).first
+  end
+
+  # we can also do the above using scope and lambda ...
+  # scope :active, -> { where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current) }
+
+
   # The public methods defined in this Sale Model will be available 
   # to each sale obj in the view partial
   # These are instance methods
