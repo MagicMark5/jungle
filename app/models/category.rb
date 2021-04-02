726824df1fef7a18cc2616500846c80002946fda
Adam Marsala <adammarsala@hotmail.com>
2021-04-02 00:58:50 +0000

    Refactor category and sale models to handle business logic

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,22 @@ class Category < ActiveRecord::Base
 
   has_many :products
 
+  # Returns the total number of products in this category
+  def total_quantity
+    category_quantity = 0 
+    self.products.each do |product|
+      category_quantity += product.quantity
+    end
+    category_quantity
+  end
+
+  # Returns the summed total value of all products in stock for this category
+  def total_value
+    category_value = 0 
+    self.products.each do |product|
+      category_value += (product.price * product.quantity)
+    end
+    category_value
+  end
+ 
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,17 @@
 class Sale < ActiveRecord::Base
+  # The public methods defined in this Sale Model will be available 
+  # to each sale obj in the view partial
+  # These are instance methods
+  def finished?
+    ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active? 
+    !finished? && !upcoming?
+  end
+
 end
